90677b7c3fd8312f2957dbcaca4b26044b4f9492
Peng Liu <littlenewton6@gmail.com>
2023-10-07 14:17:04 +0800

    [+] Initial commit.

---
+++ src/antlr-calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using antlr_calculator;
+using Antlr4.Runtime;
+using System;
+using System.Numerics;
+
+namespace antlr_calculator
+{
+    class MyCalculator
+    {
+        static void Main(string[] args)
+        {
+            string ab = @"
+193
+a = 5
+b = 6
+a+b*2
+(1+2)*3
+";
+            var inputStream = new AntlrInputStream(ab);
+            var lexer = new CalculatorLexer(inputStream);
+            var commonTokenStream = new CommonTokenStream(lexer);
+            var parser = new CalculatorParser(commonTokenStream);
+
+            var parseTree = parser.prog();
+            MyVisitor visitor = new MyVisitor();
+            visitor.Visit(parseTree);
+        }
+    }
+}
